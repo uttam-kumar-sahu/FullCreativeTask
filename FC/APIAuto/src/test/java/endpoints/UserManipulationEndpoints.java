7105c6a53859c1e91c027e5b0f02033bc57b704b
Uttam Kumar Sahu <55090431+uttam-kumar-sahu@users.noreply.github.com>
2023-07-03 14:47:07 +0530

    Update UserManipulationEndpoints.java

--- FC/APIAuto/src/test/java/endpoints/UserManipulationEndpoints.java
+++ FC/APIAuto/src/test/java/endpoints/UserManipulationEndpoints.java
@@ -11,33 +11,7 @@ import static io.restassured.RestAssured.given;
 
 public class UserManipulationEndpoints {
     static String dummyTestBaseURI = System.getProperty("sys.demo.dummyBaseURI");
-
-    public static Response listUsers(Integer page) {
-        return given().contentType("application/json").headers("contentType", "application/json")
-                .queryParam("page", page)
-                .when().get(dummyTestBaseURI+ "/api/users");
-    }
-
-    public static Response singleUser(Integer Id) {
-        return given().contentType("application/json")
-                .pathParam("id", Id)
-                .when().get(dummyTestBaseURI+ "/api/users/{id}");
-    }
-
-    public static Response singleUserGoRest(Integer Id) {
-        return given().contentType("application/json")
-                .pathParam("id", Id)
-                .when().get("https://jsonplaceholder.typicode.com/posts/{id}");
-    }
-
-    public static Response createUser(String json) {
-        RequestSpecification req = given().contentType("application/json");
-        if (json != null) {
-            req.body(json);
-        }
-        return req.when().post("https://jsonplaceholder.typicode.com/posts/");
-    }
-
+    
     public static Response createRepository(String json, Map<String, String> headers) {
         RequestSpecification req = given().headers(headers);
         if (json != null) {
